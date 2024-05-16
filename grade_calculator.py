bbcbea376daebf6b048d2c07a9b548024254cb1b
Pallavi Karemore <pallavikaremore7@gmail.com>
2024-05-16 14:30:46 +0530

    first commit

---
+++ grade_calculator.py
@@ -0,0 +1,8 @@
+def calculate_gpa(grades):
+    total_grade_point=sum(grades.values())
+    total_courses=len(grades)
+    if total_courses==0:
+        return 0
+    return total_grade_point/total_courses
+
+    
